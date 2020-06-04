aea9cd56ec0a240f42372f9ceae98d3fb67374cf
libo <905537246@qq.com>
2020-06-04 17:54:11 +0800

    Solr demo commit

---
+++ demo-web/src/main/java/com/iiva/demo/web/solr/serivce/SolrService.java
@@ -0,0 +1,10 @@
+package com.iiva.demo.web.solr.serivce;
+
+import com.iiva.demo.web.solr.entity.User;
+
+import java.util.List;
+
+public interface SolrService {
+
+    public List<User> queryUser(String condition);
+}
